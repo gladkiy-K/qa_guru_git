ec93e2f3c912dbfc207f3b56f02590022cb0f5cb
gladkiy-K <gladkiykir@yandex.com>
2024-11-09 19:04:59 +0300

    added getMessage method

--- src/main/java/guru/qa/gladkiy/Main.java
+++ src/main/java/guru/qa/gladkiy/Main.java
@@ -3,6 +3,10 @@ package guru.qa.gladkiy;
 public class Main {
     public static void main(String[] args) {
         
-        System.out.println("Hello qa.guru!");
+        System.out.println(getMessage());
+    }
+
+    public static String getMessage() {
+        return "Hello qa.guru!";
     }
 }
